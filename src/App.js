4fd5f15fe3e1208ff23ff107417aa5b4ccc96b02
Juan Galarce <j6alarce@gmail.com>
2021-01-24 10:56:10 -0300

    Se crea boton para hacer la llamada a la api, se traen los datos y se muestran por consola con fetch y async/await

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    console.log(frase[0]);
+  };
+
   return (
-    <h1>Breaking Bad</h1>
+    <Contenedor>
+      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
+    </Contenedor>
   );
 }
 
